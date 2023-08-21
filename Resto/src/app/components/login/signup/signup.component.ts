8656223bbd33cf8034e88e1da22b99854e494e6c
SriSai_G <gandusrisai@gmail.com>
2023-08-21 12:23:16 +0530

    signUp form added

--- Resto/src/app/components/login/signup/signup.component.ts
+++ Resto/src/app/components/login/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signupForm!: FormGroup
+  constructor(private formbuilder: FormBuilder, private _http:HttpClient, private _router:Router) { }
 
+  ngOnInit(): void {
+    this.signupForm = this.formbuilder.group({
+      name:[''],
+      email:[''],
+      mobile:[''],
+      password: ['']
+    })
+  }
 }
